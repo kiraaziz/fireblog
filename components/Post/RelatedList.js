3ca35edec683708179e2e336fa6b2ba7121ea1e3
kiraaziz <83905214+kiraaziz@users.noreply.github.com>
2023-05-18 04:35:58 -0700

    Update RelatedList.js

--- components/Post/RelatedList.js
+++ components/Post/RelatedList.js
@@ -1,7 +1,7 @@
 const RelatedList =({children})=> {
 
     return(
-        <div className="p-1 w-1/4 overflow-y-auto hidden sm:block bg-gray-900 overflow-x-hidden h-full">
+        <div className="p-1 w-1/4 overflow-y-auto hidden xl:block bg-gray-900 overflow-x-hidden h-full">
             {children}
         </div>
     )
