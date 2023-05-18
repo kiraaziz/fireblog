3e6906b3ec5192f5778e1e72dd61f0c9fc42314f
kiraaziz <83905214+kiraaziz@users.noreply.github.com>
2023-05-18 04:37:09 -0700

    Update MainPost.js

--- components/Post/MainPost.js
+++ components/Post/MainPost.js
@@ -2,7 +2,7 @@ const MainPost =({children})=> {
     
     return(
     
-        <div className="sm:w-3/4 w-screen overflow-y-auto bg-gray-800 overflow-x-hidden h-full">
+        <div className="xl:w-3/4 w-screen overflow-y-auto bg-gray-800 overflow-x-hidden h-full">
             {children}
         </div>
     )
