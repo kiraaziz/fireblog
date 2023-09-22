afe5386c56e9609c5da39fcca3b52fe62a41bf62
kiraaziz <83905214+kiraaziz@users.noreply.github.com>
2023-09-22 20:27:44 +0200

    Update Wraper.js

--- components/Post/Wraper.js
+++ components/Post/Wraper.js
@@ -3,7 +3,7 @@ const Wraper = ({data, setSelected, selected})=>{
     return(
         <div class="flex w-full h-max overflow-y-auto p-1 bg-gray-900">
                 {
-                    data && data.match(/\b(\w+)\b/g).map((val) => {
+                    data && [data].map((val) => {
                         return(
                             <div onClick={()=>{setSelected(val)}} class={`${selected === val? "bg-purple-500 select-border ": "bg-gray-800"} flex items-center justify-center text-sm sm:text-xl m-0.5 py-1.5 px-3  f3  rounded-xl text-white`}>
                                 {val}
